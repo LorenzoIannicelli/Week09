0026e3e76e7abfc24eb71c11efee4ea4a499cd50
LorenzoIannicelli <s341841@studenti.polito.it>
2025-11-24 22:21:01 +0100

    meteo finito, ma non funziona

--- UI/controller.py
+++ UI/controller.py
@@ -14,13 +14,29 @@ class Controller:
         self._mese = 0
 
     def handle_umidita_media(self, e):
-        pass
+        if self._mese is None :
+            self._view.create_alert("Seleziona un mese")
+        else :
+            umidita_media = self._model.get_umidita_media_mese(self._mese)
+            self._view.lst_result.controls.clear()
+            for dato in umidita_media:
+                self._view.lst_result.controls.append(ft.Text(dato[0], dato[1]))
 
-
+            self._view.update_page()
 
     def handle_sequenza(self, e):
-        pass
+        # controllare che il mese non sia None
+        if self._mese == None :
+            self._view.create_alert("Seleziona un mese")
+        else :
+            sequenza, costo = self._model.genera_sequenza(self._mese)
+            self._view.lst_result.controls.clear()
+            self._view.lst_result.controls.append(ft.Text(f'Il costo della soluzione è {costo}'))
+            for fermata in sequenza:
+                self._view.lst_result.controls.append(ft.Text(fermata))
+            self._view.update_page()
+
+
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
-
--- model/model.py
+++ model/model.py
@@ -1,3 +1,68 @@
+from database.meteo_dao import MeteoDao
+
 class Model:
     def __init__(self):
-        pass
+        self._costo_minimo = -1
+        self._soluzione_ottima = []
+
+    def get_umidita_media_mese(self, mese):
+        return MeteoDao.get_umidita_media_mese(mese)
+
+    def get_situazione_meta_mese(self, mese):
+        return MeteoDao.get_umidita_media_mese(mese)
+
+    def genera_sequenza(self, mese):
+        situazione_meta_mese = self.get_situazione_meta_mese(mese)
+        self._ricorsione([], situazione_meta_mese)
+        return self._soluzione_ottima, self._costo_minimo
+
+    def _ricorsione(self, parziale, situazioni):
+        if len(parziale) == 15:
+            print(parziale)
+            costo = self._calcola_costo(parziale)
+            if costo < self._costo_minimo or self._costo_minimo == -1:
+                self._costo_minimo = costo
+                self._soluzione_ottima = parziale
+        else :
+            giorno = len(parziale)+1
+
+            for situazione in situazioni[(giorno-1)*3 : giorno*3] :
+                if self.vincoli_soddisfatti(parziale, situazione):
+                    parziale.append(situazione)
+                    self._ricorsione(parziale, situazioni)
+                    parziale.pop()
+
+    def vincoli_soddisfatti(self, parziale, situazione) :
+        counter = 0
+        for fermata in parziale:
+            if fermata.localita == situazione.localita:
+                counter += 1
+        if counter > 6 :
+            return False
+
+        if len(parziale) > 0 and len(parziale) <= 2 :
+            if situazione.localita != parziale[0].localita :
+                return False
+
+        elif len(parziale) > 2 :
+            sequenza_finale = parziale[-3:]
+            counter = 0
+            for fermata in sequenza_finale :
+                if fermata.localita == situazione.localita :
+                    counter += 1
+            if counter < 3 and situazione.localita != sequenza_finale[-1].localita :
+                return False
+
+        return True
+
+    def _calcola_costo(self, parziale):
+        costo = 0
+        ultima_fermata = parziale[0]
+        for fermata in parziale :
+            costo += fermata.umidita
+            if fermata.localita != ultima_fermata.localita :
+                costo += 100
+
+            ultima_fermata = fermata
+
+        return costo
